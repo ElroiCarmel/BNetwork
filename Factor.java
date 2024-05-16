3256e67fd2a3d0b1dba8370933e4e197f18d3d73
elroicarmel <elroica277@gmail.com>
2024-05-16 21:32:30 +0300

    Getting the index and pr finished

--- Factor.java
+++ Factor.java
@@ -1,21 +1,66 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Factor {
     // DATA
-    private List<Variable> variables; // The ORDER of the vars is crucial
-    private double[] prob;
+    private ArrayList<Variable> variables; // The ORDER of the vars is crucial
+    private double[] probTable;
+    private int[] indexScale;
 
     // CONSTRUCTORS
     public Factor(List<Variable> variables, double[] prob) {
-        this.variables = new LinkedList<>(variables);
-        this.prob = Arrays.copyOf(prob, prob.length);
+        this.variables = new ArrayList<>(variables);
+        this.probTable = Arrays.copyOf(prob, prob.length);
+        setIndexScale();
+    }
+
+    public Factor(List<Variable> variables) {
+        this.variables = new ArrayList<>(variables);
+        int len = 1;
+        for (Variable v : variables) {
+            len = len * v.size();
+        }
+        this.probTable = new double[len];
+        setIndexScale();
+    }
+
+    private void setIndexScale() {
+        int len = variables.size();
+        this.indexScale = new int[len];
+        int product = 1;
+        for (int i = len - 1; i >= 0 ; i--) {
+            this.indexScale[i] = product;
+            product *= variables.get(i).size();
+        }
     }
 
     // METHODS
 
-    public double[] getProb() {
-        return this.prob;
+    private int getIndex(List<String> state) {
+        int[] indices = new int[state.size()];
+        for (int i = 0; i < state.size(); i++) {
+            String s = state.get(i);
+            Variable v = variables.get(i);
+            indices[i] = v.getOutcomeIndex(s);
+        }
+        int ans = 0;
+        for (int i = 0; i < state.size(); i++) {
+            ans += indices[i] * this.indexScale[i];
+        }
+        return ans;
+    }
+
+    public double getProb(List<String> state) {
+        int index = getIndex(state);
+        return this.probTable[index];
+    }
+
+
+    public double[] getTable() {
+        return this.probTable;
+    }
+
+    public List<Variable> getVariables() {
+        return this.variables;
     }
 }
